5ae0ffd3b6b06383fc12abe58c7001e8026afe4d
swolebrain <thecodingteacher@gmail.com>
2018-12-21 12:55:53 -0500

    feat: script to open/close eye

---
+++ Assets/VirtualButtonHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject cardBtn;
+    public Animation animationObj;
+    public string[] animationNames;
+    private int currentAnimation = 0;
+
+
+    public void Start()
+    {
+        cardBtn = GameObject.Find("eye_and_parts_btn");
+        cardBtn.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        //eyeAnim.GetComponent<Animator>();
+        Debug.Log("############# Btn handler registered");
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb){
+        Debug.Log("###########Playing animation index " + currentAnimation);
+        animationObj.Play(this.animationNames[currentAnimation++]);
+        currentAnimation = currentAnimation % animationNames.Length;
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb) {
+        //animationObj.Play(this.animationNames[currentAnimation++]);
+        //currentAnimation = currentAnimation % animationNames.Length;
+    }
+}
---
+++ Assets/onLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var eyeAnim = GameObject.Find("eye parts animation").GetComponent<Animation>();
+        eyeAnim.Play("contract");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
